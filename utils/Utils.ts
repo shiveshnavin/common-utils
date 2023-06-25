161330c88e95b465b5565af6b44c0a676eeff547
shiveshnavin <shiveshnavin@gmail.com>
2023-06-25 07:40:03 +0000

    update: Add random string generator

--- utils/Utils.ts
+++ utils/Utils.ts
@@ -16,6 +16,18 @@ interface ObjectWithText {
 
 export class Utils {
 
+    public static generateAlphanumericString(length = 10) {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let result = '';
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            result += characters.charAt(randomIndex);
+        }
+
+        return result;
+    }
+
     public static getTimestampOfPreviousTime(hours, minutes) {
         const now = new Date();
         const date = new Date(
