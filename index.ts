4922dc6e45793f90d6eb56efe63bfcfff89d209c
Shivesh Navin <shiveshnavin@gmail.com>
2023-08-10 17:50:32 +0530

    Add build ready

--- index.ts
+++ index.ts
@@ -3,4 +3,6 @@ export { Utils } from "./utils/Utils"
 export { WrapperPipeTask } from "./utils/WrapperPipeTask"
 export { PerformanceRecorder } from './utils/PerformanceRecorder'
 export { GetInputAsync } from './utils/GetInputAsync'
+export * from './utils/FFMpegUtils'
+export * from './utils/FileZipper'
 export * from './utils/fetch'
--- utils/GetInputAsync.ts
+++ utils/GetInputAsync.ts
@@ -1,3 +1,3 @@
 export async function GetInputAsync(key: string) {
-
+  throw new Error("Unimplemented: " + key)
 }
