0cca80a86492b663cfa28c8fd775710f11de3154
Shivesh Navin <shiveshnavin@gmail.com>
2023-08-13 17:37:59 +0530

    update: Change images to obj

--- utils/FFMpegUtils.ts
+++ utils/FFMpegUtils.ts
@@ -3,6 +3,8 @@ import { exec } from 'child_process';
 import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
 import { spawn } from 'child_process';
 import { parseArgsStringToArgv } from 'string-argv';
+import * as ffmpeg from 'fluent-ffmpeg';
+import { path as ffprobePath } from '@ffprobe-installer/ffprobe';
 
 function execute(cmd: String, onLog?: Function) {
     if (!onLog)
@@ -45,10 +47,27 @@ async function joinAudios(filePaths, outputFileName) {
     return await execute(ffmpegCommand);
 }
 
+async function getDuration(pathOfAudio): Promise<number> {
+    let durationInSeconds = await new Promise((resolve, reject) => {
+        ffmpeg.ffprobe(pathOfAudio, (err, metadata) => {
+            if (err) {
+                return reject(err)
+            }
+            if (!metadata?.format?.duration) {
+                return reject('Duration not found in ' + JSON.stringify(metadata))
+            }
+            const durationInSeconds = metadata?.format.duration
+            resolve(durationInSeconds)
+        });
+    })
+    return durationInSeconds
+}
+
 // joinAudios(['../../public/test/1.mp3', '../../public/test/2.mp3', '../../public/test/3.mp3'], '../../public/test/op.mp3')
 const FFMpegUtils = {
     execute,
-    joinAudios
+    joinAudios,
+    getDuration
 };
 export default FFMpegUtils
 
