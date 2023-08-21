f7db2450b9a7033a6386f6951d226470d6a66db4
Shivesh Navin <shiveshnavin@gmail.com>
2023-08-21 15:06:11 +0530

    update:

--- utils/InstagramGraphResponseMapper.ts
+++ utils/InstagramGraphResponseMapper.ts
@@ -2,7 +2,37 @@
 // import Source from './source.json'
 // import Target from './target.json'
 
+import bigInt from "big-integer";
+
+
+var lower = 'abcdefghijklmnopqrstuvwxyz';
+var upper = lower.toUpperCase();
+var numbers = '0123456789'
+var ig_alphabet = upper + lower + numbers + '-_'
+var bigint_alphabet = numbers + lower
+
+
 export class InstagramGraphResponseMapper {
+
+    public static toShortcode(longid: string) {
+        var o = bigInt(longid).toString(64)
+        return o.replace(/<(\d+)>|(\w)/g, (m, m1, m2) => {
+            return ig_alphabet.charAt((m1)
+                ? parseInt(m1)
+                : bigint_alphabet.indexOf(m2))
+        });
+    }
+
+    public static fromShortcode(shortcode: string) {
+        var o = shortcode.replace(/\S/g, m => {
+            var c = ig_alphabet.indexOf(m)
+            var b = bigint_alphabet.charAt(c)
+            return (b != "") ? b : `<${c}>`
+        })
+        return bigInt(o, 64).toString(10)
+    }
+
+
 
     public static mapGraphResponseToUser(
         Source: any
