224d6686609976149318bfd071bc69bbbe2feaea
Shivesh Navin <shiveshnavin@gmail.com>
2023-08-29 15:53:58 +0530

    update:

--- models/xstocks/Tick.ts
+++ models/xstocks/Tick.ts
@@ -20,7 +20,7 @@ export class Tick {
         this.high = parseFloat(body.high || body.ohlc.high);
         this.low = parseFloat(body.low || body.ohlc.low);
         this.open = parseFloat(body.open || body.ohlc.open);
-        this.close = parseFloat(body.close || body.ohlc.closea);
+        this.close = parseFloat(body.close || body.ohlc.close);
         this.volume = parseFloat(body.volume || body.volume_traded);
         this.depth = body.depth
         this.oi = parseFloat(body.oi);
