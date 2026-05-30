0287da5c7689b90a5226fba4f7a4c8b52b767beb
shiveshnavin <shiveshnavin@gmail.com>
2026-05-30 18:11:04 +0530

    Fix model

--- models/media/OriginalManuscriptModel.ts
+++ models/media/OriginalManuscriptModel.ts
@@ -51,7 +51,6 @@ export interface Bubble {
     // if both are present, mediaAbsPaths will be used and bubbleText will be ignored
     bubbleText?: BubbleText // when we need to show some text as the bubble
     bubbleHtml?: BubbleHtml
-    mediaTextPrompts?: MediaTextPrompt[]
     mediaAbsPath?: SectionMedia // when we need to show some media as the bubble
     bubbleExtra: BubbleExtra
     // optional animation that applies to the overlay itself; this is read by
